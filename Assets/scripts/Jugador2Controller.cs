f19ef5ee3cae261beadb70446485cab49d70d716
unknown <danielfelipetorreslopez@gmail.com>
2021-04-01 12:29:12 -0500

    Inicial

---
+++ Assets/scripts/Jugador2Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador2Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         
+        float movHorizontal = Input.GetAxis("Horizontal");
+        float movVertical = Input.GetAxis("Vertical");
+       
+        Vector3 direcion = new Vector3(movHorizontal,0.0f,movVertical);
+        Vector3 pos = transform.position;
+ 
+        if (Input.GetKey(KeyCode.A) ) { 
+              pos.x += 1;      
+        }
+        if (Input.GetKey(KeyCode.D) ) { 
+              pos.x -= 1;  
+        }
+        if (Input.GetKey(KeyCode.W) ) { 
+              pos.z += 1;      
+        }
+        if (Input.GetKey(KeyCode.S) ) { 
+              pos.z -= 1;  
+        }
+        transform.position = pos;
+    }
+}
---
+++ Assets/scripts/JugadorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         
+         
+        Vector3 pos = transform.position;
+ 
+        if (Input.GetKey(KeyCode.LeftArrow) ) { 
+              pos.x += 1;      
+        }
+        if (Input.GetKey(KeyCode.RightArrow) ) { 
+              pos.x -= 1;  
+        }
+        if (Input.GetKey(KeyCode.UpArrow) ) { 
+              pos.z += 1;      
+        }
+        if (Input.GetKey(KeyCode.DownArrow) ) { 
+              pos.z -= 1;  
+        }
+        transform.position = pos;
+    }
+}
